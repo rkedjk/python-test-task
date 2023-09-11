51b828dbc89de93b440580a5ad958c53e142caf1
Mustafin Radmir <rkedjk@gmail.com>
2023-09-11 18:01:48 +0300

    Trying fixing file structure

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from fastapi import FastAPI
-from geonames_db import GeoNamesDB
-from utils import *
+from app.geonames_db import GeoNamesDB
+from app.utils import get_timezone_difference_hours
 
-gndb = GeoNamesDB("geonames.sqlite")
+gndb = GeoNamesDB("databases\geonames.sqlite")
 
-app = FastAPI()
+server = FastAPI()
 
 
-@app.get("/")
+@server.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
 
-@app.get("/city/info/{geonameid}")
+@server.get("/city/info/{geonameid}")
 def read_city(geonameid: int):
     city_info = gndb.get_city_info(geonameid)
     return city_info
 
-@app.get("/city/suggest/{partial_name}")
+@server.get("/city/suggest/{partial_name}")
 def search_cities(partial_name: str):
     suggestions = gndb.get_city_name_suggestions(partial_name)
     return suggestions
 
-@app.get("/city/compare/{first_city_name}&{second_city_name}")
+@server.get("/city/compare/{first_city_name}&{second_city_name}")
 def compare_cities(first_city_name: str, second_city_name: str):
     comparison = gndb.get_cities_timezone_and_northernness_comparison(first_city_name,second_city_name)
     return comparison
