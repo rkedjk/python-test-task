eca12302644c93b2e09003747d940e5da91ca813
Mustafin Radmir <rkedjk@gmail.com>
2023-09-12 16:53:19 +0300

    Divided responses for point and cities

--- app/geonames_db.py
+++ app/geonames_db.py
@@ -1,6 +1,7 @@
 import sqlite3
 from .helpers import get_timezone_difference_hours
 
+
 class GeoNamesDB:
     def __init__(self, db_name):
         self.db_name = db_name
@@ -12,41 +13,42 @@ class GeoNamesDB:
     def close(self):
         if self.connection:
             self.connection.close()
-    
-    def _create_city_dict(self, city_info):
-        city_dict = {
-            "geonameid": city_info[0],
-            "name": city_info[1],
-            "asciiname": city_info[2],
-            "alternatenames": city_info[3],
-            "latitude": city_info[4],
-            "longitude": city_info[5],
-            "feature_class": city_info[6],
-            "feature_code": city_info[7],
-            "country_code": city_info[8],
-            "cc2": city_info[9],
-            "admin1_code": city_info[10],
-            "admin2_code": city_info[11],
-            "admin3_code": city_info[12],
-            "admin4_code": city_info[13],
-            "population": city_info[14],
-            "elevation": city_info[15],
-            "dem": city_info[16],
-            "timezone": city_info[17],
-            "modification_date": city_info[18],
+
+    def _create_point_dict(self, point_info):
+        point_dict = {
+            "geonameid": point_info[0],
+            "name": point_info[1],
+            "asciiname": point_info[2],
+            "alternatenames": point_info[3],
+            "latitude": point_info[4],
+            "longitude": point_info[5],
+            "feature_class": point_info[6],
+            "feature_code": point_info[7],
+            "country_code": point_info[8],
+            "cc2": point_info[9],
+            "admin1_code": point_info[10],
+            "admin2_code": point_info[11],
+            "admin3_code": point_info[12],
+            "admin4_code": point_info[13],
+            "population": point_info[14],
+            "elevation": point_info[15],
+            "dem": point_info[16],
+            "timezone": point_info[17],
+            "modification_date": point_info[18],
         }
-        return city_dict
-            
-    def get_city_info(self, geonameid):
+        return point_dict
+
+    def get_point_info(self, geonameid):
         try:
             self.connect()
             cursor = self.connection.cursor()
-            cursor.execute("SELECT * FROM geonames WHERE geonameid = ?", (geonameid,))
-            city_info = cursor.fetchone()
+            cursor.execute(
+                "SELECT * FROM geonames WHERE geonameid = ?", (geonameid,))
+            point_info = cursor.fetchone()
 
-            if city_info:
-                city_dict = self._create_city_dict(city_info)
-                return city_dict
+            if point_info:
+                point_dict = self._create_point_dict(point_info)
+                return point_dict
             else:
                 return None
         except sqlite3.Error as e:
@@ -54,32 +56,34 @@ class GeoNamesDB:
         finally:
             self.close()
 
-    def get_cities_on_page(self, page_number, cities_per_page):
+    def get_points_on_page(self, page_number, points_per_page):
         try:
             self.connect()
             cursor = self.connection.cursor()
-            offset = (page_number - 1) * cities_per_page
-            cursor.execute("SELECT * FROM geonames LIMIT ? OFFSET ?", (cities_per_page, offset))
-            cities_data = cursor.fetchall()
+            offset = (page_number - 1) * points_per_page
+            cursor.execute("SELECT * FROM geonames LIMIT ? OFFSET ?",
+                           (points_per_page, offset))
+            points_data = cursor.fetchall()
 
-            cities_list = []
-            for city_info in cities_data:
-                city_dict = self._create_city_dict(city_info)
-                cities_list.append(city_dict)
+            points_list = []
+            for point_info in points_data:
+                point_dict = self._create_point_dict(point_info)
+                points_list.append(point_dict)
 
-            return cities_list
+            return points_list
         except sqlite3.Error as e:
             print("Ошибка при выполнении запроса:", e)
         finally:
             self.close()
 
-    def get_city_name_suggestions(self, partial_name):
+    def get_point_name_suggestions(self, partial_name):
         try:
             self.connect()
             cursor = self.connection.cursor()
-            
+
             # Выполняем SQL-запрос для поиска городов, чьи названия начинаются с заданной части
-            cursor.execute("SELECT name FROM geonames WHERE name LIKE ? || '%'", (partial_name,))
+            cursor.execute(
+                "SELECT name FROM geonames WHERE name LIKE ? || '%'", (partial_name,))
             suggestions = cursor.fetchall()
 
             if suggestions:
@@ -98,7 +102,8 @@ class GeoNamesDB:
             cursor = self.connection.cursor()
 
             # Выполняем запрос для поиска точки по родному названию
-            cursor.execute("SELECT * FROM geonames WHERE alternatenames LIKE ?", ('%' + native_name + '%',))
+            cursor.execute(
+                "SELECT * FROM geonames WHERE alternatenames LIKE ?", ('%' + native_name + '%',))
             point_info = cursor.fetchall()
 
             if point_info:
@@ -110,66 +115,71 @@ class GeoNamesDB:
         finally:
             self.close()
 
-    def get_prioritized_city(self, city_list):
+    def get_prioritized_point(self, point_list):
         try:
-            if not city_list:
-                return {"error": "City list is empty"}
+            if not point_list:
+                return {"error": "point list is empty"}
 
             # Инициализируем переменные для хранения информации о городе с наибольшим населением
             max_population = 0
-            prioritized_city = city_list[0]
+            prioritized_point = point_list[0]
 
-            for city_info in city_list:
+            for point_info in point_list:
                 # Получаем население текущего города
-                population = city_info[14]
+                population = point_info[14]
 
                 # Если население текущего города больше, чем население ранее выбранного города,
                 # обновляем информацию о приоритетном городе
                 if population > max_population:
                     max_population = population
-                    prioritized_city = city_info
+                    prioritized_point = point_info
 
-            if prioritized_city:
-                return prioritized_city
+            if prioritized_point:
+                return prioritized_point
             else:
-                return {"error": "No prioritized city found in the list"}
+                return {"error": "No prioritized point found in the list"}
         except Exception as e:
             print("Error:", e)
-        
-    def get_cities_timezone_and_northernness_comparison(self, first_city_name_ru, second_city_name_ru):
+
+    def get_points_timezone_and_northernness_comparison(self, first_point_name_ru, second_point_name_ru):
         try:
 
             # Получаем информацию о первом городе по русскому названию
-            first_city_info = self.get_prioritized_city(self.get_point_by_native_name(first_city_name_ru))
+            first_point_info = self.get_prioritized_point(
+                self.get_point_by_native_name(first_point_name_ru))
 
             # Получаем информацию о втором городе по русскому названию
-            second_city_info = self.get_prioritized_city(self.get_point_by_native_name(second_city_name_ru))
+            second_point_info = self.get_prioritized_point(
+                self.get_point_by_native_name(second_point_name_ru))
 
-            if first_city_info and second_city_info:
-                first_city_dict = self._create_city_dict(first_city_info)
-                second_city_dict = self._create_city_dict(second_city_info)
+            if first_point_info and second_point_info:
+                first_point_dict = self._create_point_dict(first_point_info)
+                second_point_dict = self._create_point_dict(second_point_info)
 
-                northernness_first = "Northern" if first_city_dict["latitude"] > second_city_dict["latitude"] else "Southern"
-                northernness_second = "Northern" if second_city_dict["latitude"] > first_city_dict["latitude"] else "Southern"
+                northernness_first = "Northern" if first_point_dict[
+                    "latitude"] > second_point_dict["latitude"] else "Southern"
+                northernness_second = "Northern" if second_point_dict[
+                    "latitude"] > first_point_dict["latitude"] else "Southern"
 
-                timezone_difference = get_timezone_difference_hours(first_city_dict["timezone"],second_city_dict["timezone"])
-                
+                timezone_difference = get_timezone_difference_hours(
+                    first_point_dict["timezone"], second_point_dict["timezone"])
+
                 return {
-                    "first_city": {
-                        "timezone": first_city_dict["timezone"],
+                    "first_point": {
+                        "timezone": first_point_dict["timezone"],
                         "northernness": northernness_first,
-                        "city_info": first_city_dict
+                        "point_info": first_point_dict
                     },
-                    "second_city": {
-                        "timezone": second_city_dict["timezone"],
+                    "second_point": {
+                        "timezone": second_point_dict["timezone"],
                         "northernness": northernness_second,
-                        "city_info": second_city_dict
+                        "point_info": second_point_dict
                     },
                     "timezone_difference": timezone_difference
                 }
-                
+
             else:
-                return {"error": "One or both cities not found in Russia"}
+                return {"error": "One or both points not found in Russia"}
         except sqlite3.Error as e:
             print("Error executing SQL query:", e)
         finally:
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.geonames_db import GeoNamesDB
 
+gndb_city = GeoNamesDB("databases\geonames_cities.sqlite")
 gndb = GeoNamesDB("databases\geonames.sqlite")
+
 
 server = FastAPI()
 
@@ -12,20 +14,40 @@ def read_root():
 
 @server.get("/city/info/{geonameid}")
 def read_city(geonameid: int):
-    city_info = gndb.get_city_info(geonameid)
+    city_info = gndb_city.get_point_info(geonameid)
     return city_info
 
 @server.get("/city/suggest/{partial_name}")
 def search_cities(partial_name: str):
-    suggestions = gndb.get_city_name_suggestions(partial_name)
+    suggestions = gndb_city.get_point_name_suggestions(partial_name)
     return suggestions
 
 @server.get("/city/compare/{first_city_name}&{second_city_name}")
 def compare_cities(first_city_name: str, second_city_name: str):
-    comparison = gndb.get_cities_timezone_and_northernness_comparison(first_city_name,second_city_name)
+    comparison = gndb_city.get_points_timezone_and_northernness_comparison(first_city_name,second_city_name)
     return comparison
 
 @server.get("/city/listpage/p{page}&q{quantity}")
 def cities_page(page:int, quantity:int):
-    city_list = gndb.get_cities_on_page(page,quantity)
+    city_list = gndb_city.get_points_on_page(page,quantity)
+    return city_list
+
+@server.get("/point/info/{geonameid}")
+def read_city(geonameid: int):
+    city_info = gndb.get_point_info(geonameid)
+    return city_info
+
+@server.get("/point/suggest/{partial_name}")
+def search_cities(partial_name: str):
+    suggestions = gndb.get_point_name_suggestions(partial_name)
+    return suggestions
+
+@server.get("/point/compare/{first_city_name}&{second_city_name}")
+def compare_cities(first_city_name: str, second_city_name: str):
+    comparison = gndb.get_points_timezone_and_northernness_comparison(first_city_name,second_city_name)
+    return comparison
+
+@server.get("/point/listpage/p{page}&q{quantity}")
+def cities_page(page:int, quantity:int):
+    city_list = gndb.get_points_on_page(page,quantity)
     return city_list
